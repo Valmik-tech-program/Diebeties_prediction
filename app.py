40028cee93c67cc0e5c19d8d9c97b9af8a5b2e08
valmik palve <valmikpalve9322@gmail.com>
2026-09-03 11:11:42 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model=joblib.load("C:\\Users\\valmi\\OneDrive\\Desktop\\ML_excler_Notes\\excler assingment project\\Logestic_regression(Diebeties)\\logistic_model.pkl")
+scaler=joblib.load("C:\\Users\\valmi\\OneDrive\\Desktop\\ML_excler_Notes\\excler assingment project\\Logestic_regression(Diebeties)\\scaler.pkl")
+
+st.title("Diabetes Prediction")
+
+preg=st.number_input("Pregnancies")
+glu=st.number_input("Glucose")
+bp=st.number_input("Blood Pressure")
+skin=st.number_input("Skin Thickness")
+ins=st.number_input("Insulin")
+bmi=st.number_input("BMI")
+dpf=st.number_input("Diabetes Pedigree Function")
+age=st.number_input("Age")
+
+if st.button("Predict"):
+
+    data=np.array([[preg,glu,bp,skin,ins,bmi,dpf,age]])
+
+    data=scaler.transform(data)
+
+    prediction=model.predict(data)
+
+    probability=model.predict_proba(data)[0][1]
+
+    if prediction[0]==1:
+        st.error("Diabetic")
+    else:
+        st.success("Not Diabetic")
+
+    st.write("Probability:",round(probability*100,2),"%")
